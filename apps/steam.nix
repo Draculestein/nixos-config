4add1af1bf93bd3f345e5e7615ba653489e65123
Draculestein <albertjuliotheodorus@gmail.com>
2024-05-19 14:44:58 -0600

    Added steam to configuration.nix

---
+++ apps/steam.nix
@@ -0,0 +1,14 @@
+{ config, lib, pkgs, ... }:
+
+{
+  programs.steam = {
+    enable = true;
+    gamescopeSession.enable = true;
+  };
+
+  home.packages = with pkgs; [
+    mangohud
+  ];
+
+  programs.gamemode.enable = true;
+}
--- system/FlowX13/configuration.nix
+++ system/FlowX13/configuration.nix
@@ -9,6 +9,7 @@
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
       ./sound.nix
+      ../../apps/steam.nix
     ];
 
   nix = { 
