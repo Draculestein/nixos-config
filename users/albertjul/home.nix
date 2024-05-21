eb00dca8b41217f13cf9290d237e7b2c85dcf522
Draculestein <albertjuliotheodorus@gmail.com>
2024-05-20 18:14:07 -0600

    Added zoom and vesktop

--- users/albertjul/home.nix
+++ users/albertjul/home.nix
@@ -27,6 +27,8 @@
     gh
     papirus-icon-theme
     thunderbird
+    zoom-us
+    vesktop
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
