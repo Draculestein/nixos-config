76ba06364221d5f3e658759efa61d379a5a9642a
Draculestein <albertjuliotheodorus@gmail.com>
2024-05-27 15:48:40 -0600

    Added firefox

--- users/albertjul/home.nix
+++ users/albertjul/home.nix
@@ -37,6 +37,7 @@
     cachix
     vlc
     wpsoffice
+    firefox
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
