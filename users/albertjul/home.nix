d819a7329e5f2d450e444b36326af3335b618857
Draculestein <albertjuliotheodorus@gmail.com>
2024-06-07 16:27:19 -0600

    Added desktop-file-utils

--- users/albertjul/home.nix
+++ users/albertjul/home.nix
@@ -42,6 +42,7 @@
     firefox
     onlyoffice-bin
     kdePackages.okular
+    desktop-file-utils
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
