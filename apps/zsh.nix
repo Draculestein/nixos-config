f7ca4aa0221ad64fbeba5e11f60fb4c563f60831
Draculestein <albertjuliotheodorus@gmail.com>
2024-06-11 23:54:31 -0600

    Removed android paths

--- apps/zsh.nix
+++ apps/zsh.nix
@@ -7,12 +7,5 @@
       enable = true;
       theme = "robbyrussell";
     };
-
-    initExtra = 
-    ''
-    export ANDROID_HOME=$HOME/Android/Sdk
-    export PATH=$PATH:$ANDROID_HOME/emulator
-    export PATH=$PATH:$ANDROID_HOME/platform-tools
-    '';
   };
 }
