7e2367a284d5c6dcec293e5c4847c4919b844c8e
Draculestein <albertjuliotheodorus@gmail.com>
2024-07-04 00:39:26 -0600

    Added helvetica neue font to home-manager

--- users/albertjul/home.nix
+++ users/albertjul/home.nix
@@ -46,6 +46,7 @@
     nil
     nixpkgs-fmt
     nix-output-monitor
+    helvetica-neue-lt-std
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
