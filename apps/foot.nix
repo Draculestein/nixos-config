5b42d0fc28677972638aeba5103738c0f267a614
Draculestein <albertjuliotheodorus@gmail.com>
2024-07-08 00:52:28 -0600

    Added some config for foot

--- apps/foot.nix
+++ apps/foot.nix
@@ -1,5 +1,18 @@
 { config, pkgs, ... }:
 
 {
-  programs.foot.enable = true;
+  programs.foot = {
+    enable = true;
+    server.enable = true;
+
+    settings = {
+      main = {
+        title = "foot";
+        pad = "25x25";
+        font = "Hasklug Nerd Font:size=12";
+        dpi-aware = false;
+        bold-text-in-bright = true;
+      };
+    };
+  };
 }
