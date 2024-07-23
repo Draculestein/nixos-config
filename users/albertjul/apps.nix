26f024ca6a0065be77030cc05746f10ff004895d
Draculestein <albertjuliotheodorus@gmail.com>
2024-07-23 02:23:45 -0600

    Moved user apps as modules

---
+++ users/albertjul/apps.nix
@@ -0,0 +1,48 @@
+{ config, lib, pkgs, ... }:
+{
+  imports = [
+    ../modules/foot.nix
+    ../modules/bash.nix
+    ../modules/zsh
+    ../modules/brave.nix
+    ../modules/starship
+    ../modules/ssh.nix
+    ../modules/direnv.nix
+  ];
+
+  # The home.packages option allows you to install Nix packages into your
+  # environment.
+  home.packages = with pkgs; [
+    bottles
+    vscode
+    gh
+    papirus-icon-theme
+    betterbird
+    zoom-us
+    vesktop
+    rnote
+    gnome-disk-utility
+    devenv
+    vlc
+    firefox
+    onlyoffice-bin
+    kdePackages.okular
+    desktop-file-utils
+    gnome-tweaks
+    nil
+    nixpkgs-fmt
+    # # It is sometimes useful to fine-tune packages, for example, by applying
+    # # overrides. You can do that directly here, just don't forget the
+    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
+    # # fonts?
+    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
+
+    # # You can also create simple shell scripts directly inside your
+    # # configuration. For example, this adds a command 'my-hello' to your
+    # # environment:
+    # (pkgs.writeShellScriptBin "my-hello" ''
+    #   echo "Hello, ${config.home.username}!"
+    # '')
+  ];
+
+}
