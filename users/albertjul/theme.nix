086d362059433499d0dd6115dc879b0b2ec84c4b
Draculestein <albertjuliotheodorus@gmail.com>
2024-07-23 02:31:49 -0600

    Fixed icon theme settings

--- users/albertjul/theme.nix
+++ users/albertjul/theme.nix
@@ -12,15 +12,10 @@
       package = pkgs.gnome-themes-extra;
       name = "Adwaita-dark";
     };
-  };
 
-  home.extraOptions = {
-    gtk = {
-      enable = true;
-      iconTheme = {
-        name = "Papirus Icon Theme";
-        package = pkgs.papirus-icon-theme;
-      };
+    iconTheme = {
+      name = "Papirus Icon Theme";
+      package = pkgs.papirus-icon-theme;
     };
   };
 }
