5c3221b91602c7b286aa6ff86e5bcbc5cb9d2321
Draculestein <albertjuliotheodorus@gmail.com>
2024-07-23 02:23:55 -0600

    Cleaned up home.nix

--- users/albertjul/home.nix
+++ users/albertjul/home.nix
@@ -2,12 +2,10 @@
 
 {
   imports = [
-    ../../apps/foot.nix
-    ../../apps/bash.nix
-    ../../apps/zsh
-    ../../apps/brave.nix
-    ../../apps/starship
-    ../../apps/ssh.nix
+    ./apps.nix
+    ./theme.nix
+    ./files.nix
+    ./fonts.nix
   ];
 
   # Home Manager needs a bit of information about you and the paths it should
@@ -24,66 +22,9 @@
   # release notes.
   home.stateVersion = "23.11"; # Please read the comment before changing.
 
-  # The home.packages option allows you to install Nix packages into your
-  # environment.
-  home.packages = with pkgs; [
-    bottles
-    vscode
-    gh
-    papirus-icon-theme
-    betterbird
-    zoom-us
-    vesktop
-    rnote
-    gnome-disk-utility
-    devenv
-    vlc
-    firefox
-    onlyoffice-bin
-    kdePackages.okular
-    desktop-file-utils
-    gnome-tweaks
-    nil
-    nixpkgs-fmt
-    helvetica-neue-lt-std
-    # # It is sometimes useful to fine-tune packages, for example, by applying
-    # # overrides. You can do that directly here, just don't forget the
-    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
-    # # fonts?
-    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
-
-    # # You can also create simple shell scripts directly inside your
-    # # configuration. For example, this adds a command 'my-hello' to your
-    # # environment:
-    # (pkgs.writeShellScriptBin "my-hello" ''
-    #   echo "Hello, ${config.home.username}!"
-    # '')
-  ];
-
-  fonts.fontconfig.enable = true;
-
   targets.genericLinux.enable = true;
   xdg.mime.enable = true;
   xdg.systemDirs.data = [ "${config.home.homeDirectory}/.nix-profile/share/applications" ];
-
-  qt = {
-    enable = true;
-    platformTheme.name = "adwaita";
-    style.name = "adwaita-dark";
-  };
-
-  gtk = {
-    enable = true;
-    theme = {
-      package = pkgs.gnome-themes-extra;
-      name = "Adwaita-dark";
-    };
-  };
-
-  programs.direnv = {
-    enable = true;
-    nix-direnv.enable = true;
-  };
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
