224b28c9d87a85003214d4b0c2c0ea4d8bedcf5c
Draculestein <albertjuliotheodorus@gmail.com>
2024-07-23 21:14:18 -0600

    Finished setting up restic

--- system/modules/restic.nix
+++ system/modules/restic.nix
@@ -1,3 +1,45 @@
 { config, pkgs, ... }:
+let
+  username = "albertjul";
+  secretsPath = "/run/secrets/restic";
+in
+{
+  services.restic.backups.albertjul-FlowX13-B2backup = {
+    initialize = true;
+    user = "${username}";
+    environmentFile = "${secretsPath}/environment";
+    repositoryFile = "${secretsPath}/repository";
+    passwordFile = "${secretsPath}/password";
 
-{ }
+    pruneOpts = [
+      "--keep-daily 7"
+      "--keep-weekly 4"
+      "--keep-monthly 12"
+      "--keep-yearly 2"
+    ];
+
+    paths = [
+      "/home/${username}"
+    ];
+
+    exclude = [
+      "/home/${username}/.cache"
+      "/home/${userName}/.local/share/Trash"
+      "/home/${userName}/.var/app/*/cache"
+    ];
+
+    extraBackupArgs = [
+      "--exclude-caches"
+    ];
+
+    # Necessary to prevent locks from persisting indefinitely. See more:
+    # https://forum.restic.net/t/restic-unlock-automation/5511
+    backupPrepareCommand = "${pkgs.restic}/bin/restic unlock";
+
+    timerConfig = {
+      OnCalendar = "15:00";
+      Persistent = true;
+    };
+  };
+
+}
