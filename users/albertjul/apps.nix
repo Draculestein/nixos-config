4b8eb065965d626b97310dde427f88b7112de792
Draculestein <albertjuliotheodorus@gmail.com>
2024-07-26 19:05:06 -0600

    Removed bluemail (does not work)

--- users/albertjul/apps.nix
+++ users/albertjul/apps.nix
@@ -31,7 +31,6 @@
     gnome-tweaks
     nil
     nixpkgs-fmt
-    bluemail
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
