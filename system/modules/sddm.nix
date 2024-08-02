906b6b55291613c09d634b20c801ea6161b33569
Draculestein <albertjuliotheodorus@gmail.com>
2024-08-02 01:38:31 -0600

    Added catpuchin corner theme for sddm

--- system/modules/sddm.nix
+++ system/modules/sddm.nix
@@ -1,7 +1,9 @@
 { config, lib, pkgs, ... }:
 {
+  environment.systemPackages = with pkgs; [ catppuccin-sddm-corners ];
   services.displayManager.sddm = {
     enable = true;
     wayland.enable = true;
+    theme = "catppuccin-sddm-corners";
   };
 }
