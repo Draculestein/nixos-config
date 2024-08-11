d5dc51451f40255308e2c6435b7adc0c7fbc80d1
Draculestein <albertjuliotheodorus@gmail.com>
2024-08-11 01:25:13 -0600

    Enabled hyprland

--- system/modules/hyprland.nix
+++ system/modules/hyprland.nix
@@ -4,4 +4,10 @@
     substituters = [ "https://hyprland.cachix.org" ];
     trusted-public-keys = [ "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc=" ];
   };
+
+  programs.hyprland = {
+    enable = true;
+    package = hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
+    xwayland.enable = true;
+  };
 }
