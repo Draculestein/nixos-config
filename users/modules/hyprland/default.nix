234381c53fe783a1c9796e727f03158b6d5c4f08
Draculestein <albertjuliotheodorus@gmail.com>
2024-08-31 22:17:07 -0600

    Added environment variables and autostart

--- users/modules/hyprland/default.nix
+++ users/modules/hyprland/default.nix
@@ -3,9 +3,26 @@
   windows.windowsManager.hyprland = {
     enable = true;
     package = inputs.hyprland.packages.${pkgs.system}.hyprland;
+    systemd.enable = true;
+
     settings = {
+      # ======== Environment ========
+      env = [
+        "GDK_BACKEND, wayland,x11,*"
+        "QT_QPA_PLATFORM, wayland;xcb"
+        "SDL_VIDEODRIVER, wayland"
+        "QT_AUTO_SCREEN_SCALE_FACTOR, 1"
+        "XDG_CURRENT_DESKTOP, Hyprland"
+        "XDG_SESSION_TYPE, wayland"
+        "XDG_SESSION_DESKTOP, Hyprland"
+        "XCURSOR_SIZE, 24"
+        "HYPRCURSOR_SIZE, 24"
+      ];
 
-
+      # ======== Autostart ========
+      exec-once = [
+        "ags"
+      ];
 
       # ======== Monitor ========
       monitor = [
@@ -23,6 +40,7 @@
         "$mod, B, exec, $browser"
         "$mod, T, exec, $terminal"
         "$mod, E, exec, $fileManager"
+        "$mod, Q, killactive"
       ];
 
       bindm = [
