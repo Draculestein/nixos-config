ec64e5e0b17426f330bd934a2f7d3de843cdcbaa
Draculestein <albertjuliotheodorus@gmail.com>
2024-09-03 21:15:11 -0600

    Zed editor won't run

--- users/albertjul/apps.nix
+++ users/albertjul/apps.nix
@@ -40,7 +40,6 @@
     nixd
     path-of-building
     bitwarden-desktop
-    zed-editor
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
