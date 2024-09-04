b52e273f8426cac5d8eb95f5c29f0bb40b9e81e6
Draculestein <albertjuliotheodorus@gmail.com>
2024-09-04 00:00:01 -0600

    Removed stylix completely

--- users/albertjul/theme.nix
+++ users/albertjul/theme.nix
@@ -1,39 +1,11 @@
 { config, lib, pkgs, inputs, ... }:
 {
-  imports = [
-    inputs.stylix.homeManagerModules.stylix
-  ];
-
-  stylix = {
-    enable = false;
-    image = ./desktop.png;
-    polarity = "dark";
-    base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-macchiato.yaml";
-
-    cursor = {
-      package = pkgs.vimix-cursor-theme;
-      name = "Vimix-Cursors";
-      size = 12;
-    };
-
-    fonts = {
-      monospace = {
-        name = "Hasklug Nerd Font";
-        package = pkgs.nerdfonts.override { fonts = [ "Hasklig" ]; };
-      };
-    };
-
-    targets = {
-      vscode.enable = false;
-      hyprpaper.enable = false;
-    };
-  };
 
   home.pointerCursor = {
-    gtk.enable = true;
-    package = pkgs.vimix-cursor-theme;
-    name = "Vimix-Cursors";
-    size = 12;
+     gtk.enable = true;
+     package = pkgs.vimix-cursor-theme;
+     name = "Vimix-Cursors";
+     size = 12;
   };
 
   qt = {
