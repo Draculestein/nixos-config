ce17f42a952754f4612ed9d868275f24e8ba8d9b
Draculestein <albertjuliotheodorus@gmail.com>
2024-09-11 01:15:06 -0600

    Added gh settings

--- users/modules/gh.nix
+++ users/modules/gh.nix
@@ -3,5 +3,17 @@
   programs.gh = {
     enable = true;
     gitCredentialHelper.enable = true;
+
+    settings = {
+      git_protocol = "https";
+    
+      prompt = "enabled";
+    
+      aliases = {
+        co = "pr checkout";
+        pv = "pr view";
+      };
+    };
+    
   };
 }
