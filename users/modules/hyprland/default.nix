a94fc6b87e55b0b53ebd93663ab729cc235a3edd
Draculestein <albertjuliotheodorus@gmail.com>
2024-09-15 02:41:56 -0600

    Added audio controls

--- users/modules/hyprland/default.nix
+++ users/modules/hyprland/default.nix
@@ -173,6 +173,11 @@
           ", XF86KbdBrightnessDown, exec, brightnessctl -d asus::kbd_backlight set 33%-"
           ", XF86MonBrightnessUp, exec, brightnessctl set 10%+"
           ", XF86MonBrightnessDown, exec, brightnessctl set 10%-"
+          ", XF86AudioRaiseVolume, exec, wpctl set-volume -l 1.0 @DEFAULT_AUDIO_SINK@ 5%+"
+          ", XF86AudioLowerVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"
+          ", XF86AudioMute, exec, wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"
+          ", XF86AudioMicMute, exec, wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle"
+
 
         ]
         ++ builtins.concatLists ws_bindings;
