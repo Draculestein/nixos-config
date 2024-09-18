04a2a68bf298dfe3e1b9358ffb9456a73924e0e4
Draculestein <albertjuliotheodorus@gmail.com>
2024-09-18 17:24:55 -0600

    Added hypridle and iio-hyprland to autostart

--- users/modules/hyprland/autostart.nix
+++ users/modules/hyprland/autostart.nix
@@ -5,7 +5,9 @@
       # ======== Autostart ========
       exec-once = [
         "systemctl --user import-environment PATH &" 
+        "hypridle &"
         "hyprpaper &"
+        "iio-hyprland &"
         "systemctl start --user my-polkit-gnome-authentication-agent-1 &"
         "systemctl start --user start-gnome-keyring-daemon &"
         "ags"
