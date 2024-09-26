9e300ff264b4a9f4076a7e919ef92b97ad2c5298
Draculestein <albertjuliotheodorus@gmail.com>
2024-09-25 22:21:04 -0600

    Fixed bug with nix settings

--- users/modules/devenv.nix
+++ users/modules/devenv.nix
@@ -5,12 +5,15 @@
   ];
 
 
-  nix.settings = {
-    substituters = [
-      "https://nixpkgs-python.cachix.org"
-    ];
-    trusted-public-keys = [
-      "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw= nixpkgs-python.cachix.org-1:hxjI7pFxTyuTHn2NkvWCrAUcNZLNS3ZAvfYNuYifcEU="
-    ];
+  nix = {
+    package = pkgs.nix;
+    settings = {
+      substituters = [
+        "https://nixpkgs-python.cachix.org"
+      ];
+      trusted-public-keys = [
+        "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw= nixpkgs-python.cachix.org-1:hxjI7pFxTyuTHn2NkvWCrAUcNZLNS3ZAvfYNuYifcEU="
+      ];
+    };
   };
 }
