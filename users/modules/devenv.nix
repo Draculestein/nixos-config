ad346e69eda125938d84816535afe4d5a825886c
Draculestein <albertjuliotheodorus@gmail.com>
2024-09-26 10:31:28 -0600

    Removed nix extraSubstituters

--- users/modules/devenv.nix
+++ users/modules/devenv.nix
@@ -4,16 +4,4 @@
     devenv
   ];
 
-
-  nix = {
-    package = pkgs.nix;
-    settings = {
-      substituters = [
-        "https://nixpkgs-python.cachix.org"
-      ];
-      trusted-public-keys = [
-        "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw= nixpkgs-python.cachix.org-1:hxjI7pFxTyuTHn2NkvWCrAUcNZLNS3ZAvfYNuYifcEU="
-      ];
-    };
-  };
 }
