8770888ade95ce9d97b7431088e3bbb5254425b4
Draculestein <albertjuliotheodorus@gmail.com>
2024-09-29 14:49:16 -0600

    Changed eww nix config to use home manager

--- users/modules/eww/default.nix
+++ users/modules/eww/default.nix
@@ -1,14 +1,8 @@
 {config, lib,  pkgs, ...}:
 {
-  home.packages = with pkgs;[
-    eww
-  ];
-
-  home.file = {
-    ".config/eww" = {
-      enable = true;
-      source = ../eww;
-      executable = true;
-    };
+  programs.eww = {
+    enable = true;
+    enableZshIntegration = true;
+    configDir = ../eww;
   };
 }
