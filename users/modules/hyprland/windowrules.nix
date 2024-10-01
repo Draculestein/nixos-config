c3b6e9c31be7676848fd47ca7738d1277bbb6897
Draculestein <albertjuliotheodorus@gmail.com>
2024-10-01 14:42:35 -0600

    Added windowrules to disable hyprbars for some apps

--- users/modules/hyprland/windowrules.nix
+++ users/modules/hyprland/windowrules.nix
@@ -5,10 +5,17 @@
     windowrulev2 = [
       "suppressevent maximize, class:.*" # You'll probably like this.
       "float, class:(xdg-desktop-portal-)(.*)"
+      "plugin:hyprbars:nobar, class:(xdg-desktop-portal-)(.*)"
+
       "float, class:(org.gnome.NautilusPreviewer)"
+      "plugin:hyprbrars:nobar, class:(org.gnome.NautilusPreviewer)"
       "float, class:(flameshot)"
       "float, class:(clipse)"
       "size 622 652, class:(clipse)"
+
+      "plugin:hyprbars:nobar, class:(brave-browser)"
+      "plugin:hyprbars:nobar, class:(thunderbird)"
+
     ];
   };
 }
