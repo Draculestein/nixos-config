f5f2647512191e2000195642cf82d8781263f94a
Draculestein <albertjuliotheodorus@gmail.com>
2024-10-19 19:26:45 -0600

    Removed ags since it failed to download from gnome gitlab

--- users/modules/ags/default.nix
+++ users/modules/ags/default.nix
@@ -4,7 +4,7 @@
   imports = [ inputs.ags.homeManagerModules.default ];
 
   programs.ags = {
-    enable = true;
+    enable = false;
 
     # null or path, leave as null if you don't want hm to manage the config
     configDir = null; # Using the default ags path ($XDG_HOME/.config/ags)
@@ -12,7 +12,7 @@
     # additional packages to add to gjs's runtime
     extraPackages = with pkgs; [
       gtksourceview
-      webkitgtk
+      webkitgtk_6_0
       accountsservice
     ];
   };
