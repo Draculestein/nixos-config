56c570f0a36c975512cfe25d0d20d0ca555d6c20
Draculestein <albertjuliotheodorus@gmail.com>
2024-10-25 15:40:50 -0600

    Added tlp settings

--- system/modules/tlp.nix
+++ system/modules/tlp.nix
@@ -1,4 +1,19 @@
-{config, lib, pkgs, ... }:
+{ config, lib, pkgs, ... }:
 {
   services.tlp.enable = true;
+  services.tlp.settings = {
+    DEVICES_TO_DISABLE_ON_STARTUP = "bluetooth";
+    RUNTIME_PM_ON_AC = "on";
+    RUNTIME_PM_ON_BAT = "auto";
+
+    # CPU Settings
+    CPU_DRIVER_OPMODE_ON_AC = "active";
+    CPU_DRIVER_OPMODE_ON_BAT = "active";
+
+    CPU_ENERGY_PERF_POLICY_ON_AC = "balance_performance";
+    CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+
+    CPU_BOOST_ON_AC = 1;
+    CPU_BOOST_ON_BAT = 0;
+  };
 }
