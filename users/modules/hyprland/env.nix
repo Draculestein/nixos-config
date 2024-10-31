2a7d26d05bb38d71264a8af9219887c629fb676b
Draculestein <albertjuliotheodorus@gmail.com>
2024-10-31 17:17:31 -0600

    Added clutter backend env var

--- users/modules/hyprland/env.nix
+++ users/modules/hyprland/env.nix
@@ -3,15 +3,16 @@
   wayland.windowManager.hyprland .settings = {
     # ======== Environment ========
     env = [
-      "GDK_BACKEND, wayland,x11,*"
-      "QT_QPA_PLATFORM, wayland;xcb"
-      "SDL_VIDEODRIVER, wayland"
-      "QT_AUTO_SCREEN_SCALE_FACTOR, 1"
-      "XDG_CURRENT_DESKTOP, Hyprland"
-      "XDG_SESSION_TYPE, wayland"
-      "XDG_SESSION_DESKTOP, Hyprland"
-      "XCURSOR_SIZE, 24"
-      "SSH_AUTH_SOCK, $XDG_RUNTIME_DIR/keyring/ssh"
+      "GDK_BACKEND,wayland,x11,*"
+      "QT_QPA_PLATFORM,wayland;xcb"
+      "SDL_VIDEODRIVER,wayland"
+      "CLUTTER_BACKEND,wayland"
+      "QT_AUTO_SCREEN_SCALE_FACTOR,1"
+      "XDG_CURRENT_DESKTOP,Hyprland"
+      "XDG_SESSION_TYPE,wayland"
+      "XDG_SESSION_DESKTOP,Hyprland"
+      "XCURSOR_SIZE,24"
+      "SSH_AUTH_SOCK,$XDG_RUNTIME_DIR/keyring/ssh"
     ];
   };
 }
