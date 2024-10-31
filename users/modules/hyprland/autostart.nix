63b194787202a8ac471db754270c1d22f96cca86
Draculestein <albertjuliotheodorus@gmail.com>
2024-10-31 17:17:47 -0600

    Disabled eww

--- users/modules/hyprland/autostart.nix
+++ users/modules/hyprland/autostart.nix
@@ -3,6 +3,7 @@
   wayland.windowManager.hyprland.settings = {
     # ======== Autostart ========
     exec-once = [
+      "dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP &"
       "systemctl --user import-environment PATH &"
       "hypridle &"
       "hyprpaper &"
@@ -13,7 +14,7 @@
       "swaync &"
       "swayosd-server &"
       "waybar"
-      "$HOME/.nix-profile/bin/eww daemon"
+      # "$HOME/.nix-profile/bin/eww daemon"
     ];
   };
 }
