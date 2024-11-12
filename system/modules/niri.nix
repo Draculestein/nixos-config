b51ae373ee2c50a7f22d510dc83012dbd654fb38
Draculestein <albertjuliotheodorus@gmail.com>
2024-11-11 21:56:57 -0700

    Added and installed niri

--- system/modules/niri.nix
+++ system/modules/niri.nix
@@ -3,7 +3,6 @@
   nixpkgs.overlays = [ inputs.niri.overlays.niri ];
 
   programs.niri.enable = true;
-  programs.niri.package = pkgs.niri-unstable;
-  programs.niri.environment."NIXOS_OZONE_WL" = 1;
+  # programs.niri.package = pkgs.niri-unstable;
 
 }
