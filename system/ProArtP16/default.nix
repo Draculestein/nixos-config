5ea62164f8a97c83a485328790fd4088500b6d9f
Draculestein <albertjuliotheodorus@gmail.com>
2024-11-30 21:12:31 -0700

    Added P16 config

---
+++ system/ProArtP16/default.nix
@@ -0,0 +1,41 @@
+{ config, lib, pkgs, ... } : {
+  imports = [
+    ./configuration.nix
+
+    # Hardware
+    ../modules/zswap.nix
+    ./gpu.nix
+    ./disk.nix
+    ../modules/asus.nix
+    ../modules/wacom.nix
+    ../modules/xboxController.nix
+    # ../modules/powertop.nix
+    # ../modules/autocpu-freq.nix
+    ../modules/tlp.nix
+    ../modules/blueman.nix
+
+    # Display Manager and DE
+    # ../modules/gdm.nix
+    # ../modules/gnome.nix
+    ../modules/sddm.nix
+    ../modules/plasma6.nix
+    # ../modules/hyprland.nix
+    # ../modules/gnome-polkit.nix
+    ../modules/niri.nix
+
+    # Software
+    ./fonts.nix
+    ./packages.nix
+    ../modules/printing.nix
+    ../modules/logitech.nix
+    ../modules/nh.nix
+    ../modules/nixSettings.nix
+    ../modules/virtualization.nix
+    ../modules/restic.nix
+    ../modules/sops.nix
+    ../modules/sound.nix
+    ../modules/steam.nix
+    ../modules/flatpak.nix
+    # ../modules/ollama.nix
+  ];
+}
