0ef29f07fbf3ffc56228fc5c480d374292751768
Draculestein <albertjuliotheodorus@gmail.com>
2024-12-10 02:18:08 -0700

    Changed from tlp to ppd

--- system/ProArtP16/default.nix
+++ system/ProArtP16/default.nix
@@ -10,7 +10,7 @@
     ../modules/asus.nix
     ../modules/wacom.nix
     ../modules/xboxController.nix
-    ../modules/tlp.nix
+    ../modules/powerprofilesdaemon.nix
     ../modules/blueman.nix
 
     # Display Manager and DE
