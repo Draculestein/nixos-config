1fc694791d620090a35412063cd7c2d22a87a256
Draculestein <albertjuliotheodorus@gmail.com>
2025-01-07 11:07:07 -0700

    Disabled kopia

--- users/albertjul/apps.nix
+++ users/albertjul/apps.nix
@@ -48,7 +48,6 @@
     file-roller
     seahorse
     d-spy
-    kopia
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
