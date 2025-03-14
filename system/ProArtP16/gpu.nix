294e754593d1fc7fbce4e4bd115b344a327b9374
Draculestein <albertjuliotheodorus@gmail.com>
2025-03-13 22:27:14 -0600

    Changed GPU driver to stable

--- system/ProArtP16/gpu.nix
+++ system/ProArtP16/gpu.nix
@@ -16,7 +16,7 @@
 
   services.xserver.videoDrivers = [ "nvidia" ];
   hardware.nvidia = {
-    package = config.boot.kernelPackages.nvidiaPackages.production;
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
     modesetting.enable = true;
     nvidiaSettings = true;
 
