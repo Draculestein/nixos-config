835589079f4dce2a302c57ce15c850c7451e7203
Draculestein <albertjuliotheodorus@gmail.com>
2025-03-30 21:35:39 -0600

    Added epson drivers

--- system/modules/printing.nix
+++ system/modules/printing.nix
@@ -1,7 +1,13 @@
 { config, lib, pkgs, ... }:
 {
   # Enable CUPS to print documents.
-  services.printing.enable = true;
+  services.printing = {
+    enable = true;
+    drivers = with pkgs;[
+      epson-escpr
+      epsonscan2
+    ];
+  };
 
   services.avahi = {
     enable = true;
