a75bf127c8e043ec87d2c84f850207427b7862dc
Draculestein <albertjuliotheodorus@gmail.com>
2025-04-05 19:56:18 -0600

    Added devenv cachix keys for ProArtP16

--- system/ProArtP16/configuration.nix
+++ system/ProArtP16/configuration.nix
@@ -88,6 +88,11 @@
     LABEL="asusd_end"
   '';
 
+  nix.settings = {
+    substituters = [ "https://devenv.cachix.org" ];
+    trusted-public-keys = [ "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw= nixpkgs-python.cachix.org-1:hxjI7pFxTyuTHn2NkvWCrAUcNZLNS3ZAvfYNuYifcEU=" ];
+  };
+
 
   environment.sessionVariables.NIXOS_OZONE_WL = "1";
 
