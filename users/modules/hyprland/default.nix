9ab75d5affba8bf740842e0407488afe5596076d
Draculestein <albertjuliotheodorus@gmail.com>
2025-04-28 13:34:09 -0600

    Deleted some nix files related to hyprland config

--- users/modules/hyprland/default.nix
+++ users/modules/hyprland/default.nix
@@ -1,31 +1,14 @@
 { config, pkgs, lib, inputs, ... }:
 {
-  imports = [
-    ./lockscreen.nix
-    ./config.nix
-    ./env.nix
-    ./autostart.nix
-    ./binds.nix
-    ./polkit.nix
-    ./hyprpaper.nix
-    ./windowrules.nix
 
-    ../flameshot.nix
-    ../swaync
-    ../rofi
-    ../swayosd.nix
-    ../waybar.nix
-
-  ];
-
-  home.packages = with pkgs; [
-    brightnessctl
-    playerctl
-    blueman
-    wl-clipboard
-    clipse
-    kooha
-  ];
+  # home.packages = with pkgs; [
+  #   brightnessctl
+  #   playerctl
+  #   blueman
+  #   wl-clipboard
+  #   clipse
+  #   kooha
+  # ];
 
   wayland.windowManager.hyprland = {
     enable = true;
