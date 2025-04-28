18cf1ea53b7ef44ffe1857ccd62119eeda604e1e
Draculestein <albertjuliotheodorus@gmail.com>
2025-04-28 00:46:58 -0600

    Deleted niri

--- system/modules/niri.nix
+++
@@ -1,8 +0,0 @@
-{ config, pkgs, lib, inputs, ... }: {
-
-  nixpkgs.overlays = [ inputs.niri.overlays.niri ];
-
-  programs.niri.enable = true;
-  # programs.niri.package = pkgs.niri-unstable;
-
-}
--- users/modules/niri/default.nix
+++
@@ -1,7 +0,0 @@
-{config, lib, pkgs, inputs, ...}:
-{
-  imports = [
-    inputs.niri.homeModules.niri
-  ];
-  
-}
