fa93445dd29da5ed4e8ca715d7ab9bb5526d7807
Draculestein <albertjuliotheodorus@gmail.com>
2025-04-28 14:05:30 -0600

    Disabled systemd integration since it breaks xdg-desktop-portal

--- system/modules/hyprland.nix
+++ system/modules/hyprland.nix
@@ -8,7 +8,6 @@
   programs.hyprland = {
     enable = true;
     package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
-    # make sure to also set the portal package, so that they are in sync
     portalPackage = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
     xwayland.enable = true;
 
@@ -24,32 +23,5 @@
 
   security.polkit.enable = true;
   security.pam.services.hyprlock = { };
-
-
-  xdg.portal = {
-    enable = true;
-    xdgOpenUsePortal = false;
-    
-    # Fallback portal
-    extraPortals = with pkgs; [
-      xdg-desktop-portal-gtk
-      inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland
-    ];
-
-    config = {
-
-      # Portal for hyprland
-      hyprland = {
-        default = [
-          "hyprland"
-          "gtk"
-        ];
-        "org.freedesktop.impl.portal.Secret" = [ "gnome-keyring" ];
-        "org.freedesktop.portal.FileChooser" = [ "gtk" ];
-      };
-
-    };
-  };
-
 
 }
--- users/modules/hyprland/default.nix
+++ users/modules/hyprland/default.nix
@@ -13,11 +13,9 @@
   wayland.windowManager.hyprland = {
     enable = true;
 
-    package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
-    portalPackage = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
-    systemd.enable = true;
-    systemd.enableXdgAutostart = true;
+    package = null;
+    portalPackage = null;
 
-    systemd.variables = [ "--all" ];
+    xwayland.enable = true;
   };
 }
