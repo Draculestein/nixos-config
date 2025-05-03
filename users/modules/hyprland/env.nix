99d4ef50d25269e5eb5b0871e17de6121d81e4ae
Draculestein <albertjuliotheodorus@gmail.com>
2025-05-03 01:40:12 -0600

    Unset some env variables since UWSM will set them

--- users/modules/hyprland/env.nix
+++ users/modules/hyprland/env.nix
@@ -8,9 +8,6 @@
       "SDL_VIDEODRIVER,wayland"
       "CLUTTER_BACKEND,wayland"
       "QT_AUTO_SCREEN_SCALE_FACTOR,1"
-      "XDG_CURRENT_DESKTOP,Hyprland"
-      "XDG_SESSION_TYPE,wayland"
-      "XDG_SESSION_DESKTOP,Hyprland"
       "XCURSOR_SIZE,24"
       "SSH_AUTH_SOCK,$XDG_RUNTIME_DIR/keyring/ssh"
     ];
