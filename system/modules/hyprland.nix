5b234408af090b441962b69679576cfbf4d8cd72
Draculestein <albertjuliotheodorus@gmail.com>
2025-05-03 11:08:16 -0600

    Added uwsm config entry for hyprland

--- system/modules/hyprland.nix
+++ system/modules/hyprland.nix
@@ -14,6 +14,17 @@
     withUWSM = true;
   };
 
+  programs.uwsm = {
+    enable = true;
+    waylandCompositors = {
+      hyprland = {
+        prettyName = "Hyprland";
+        comment = "Hyprland compositor managed by UWSM";
+        binPath = "/run/current-system/sw/bin/Hyprland";
+      };
+    };
+  };
+
   environment.systemPackages = with pkgs; [
     xdg-utils
     kitty
