fa90c32860362c6a9e6b2adb11883178dac46372
Draculestein <albertjuliotheodorus@gmail.com>
2025-05-03 01:03:07 -0600

    Changed hyprpaper service to only start if on Hyprland

--- users/modules/hyprland/hyprpaper.nix
+++ users/modules/hyprland/hyprpaper.nix
@@ -7,4 +7,10 @@
       splash = false;
     };
   };
+
+  systemd.user.services.hyprpaper = {
+    Unit = {
+      ConditionEnvironment = lib.mkForce [ "WAYLAND_DISPLAY" "XDG_CURRENT_DESKTOP=Hyprland" ];
+    };
+  };
 }
