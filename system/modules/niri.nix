6d7433dd3828e624698055b03cccb043f7a931fd
Draculestein <albertjuliotheodorus@gmail.com>
2025-05-18 14:52:48 -0600

    Changed niri version to unstable

--- system/modules/niri.nix
+++ system/modules/niri.nix
@@ -2,7 +2,7 @@
   nixpkgs.overlays = [ inputs.niri.overlays.niri ];
   programs.niri = {
     enable = true;
-    package = pkgs.niri-stable;
+    package = pkgs.niri-unstable;
   };
   systemd.user.services.xwayland-satellite.wantedBy = [ "graphical-session.target" ];
   security.pam.services.swaylock = { };
