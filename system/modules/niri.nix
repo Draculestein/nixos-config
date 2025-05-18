f9946910125971b263363145e836803cf61f6d00
Draculestein <albertjuliotheodorus@gmail.com>
2025-05-18 16:10:20 -0600

    Removed xwayland-satellite from system niri module

--- system/modules/niri.nix
+++ system/modules/niri.nix
@@ -4,7 +4,6 @@
     enable = true;
     package = pkgs.niri-unstable;
   };
-  systemd.user.services.xwayland-satellite.wantedBy = [ "graphical-session.target" ];
   security.pam.services.swaylock = { };
 }
 
