6603da9cdf2b556c5f6184f92fc6720b04783317
Draculestein <albertjuliotheodorus@gmail.com>
2025-05-17 23:07:14 -0600

    Changed niri version to stable and added swaylock and xwayland satellite

--- system/modules/niri.nix
+++ system/modules/niri.nix
@@ -2,7 +2,9 @@
   nixpkgs.overlays = [ inputs.niri.overlays.niri ];
   programs.niri = {
     enable = true;
-    package = pkgs.niri-unstable;
+    package = pkgs.niri-stable;
   };
+  systemd.user.services.xwayland-satellite.wantedBy = [ "graphical-session.target" ];
+  security.pam.services.swaylock = { };
 }
 
