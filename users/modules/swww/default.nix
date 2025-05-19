dea5539e88e39bead32a4545e500343ec212cf6e
Draculestein <albertjuliotheodorus@gmail.com>
2025-05-19 16:23:41 -0600

    Reformat

--- users/modules/swww/default.nix
+++ users/modules/swww/default.nix
@@ -1,6 +1,6 @@
-{config, lib, pkgs, inputs, ...}: {
-    services.swww = {
-      enable = true;
-      package = inputs.swww.packages.${pkgs.system}.swww;
-    };
+{ config, lib, pkgs, inputs, ... }: {
+  services.swww = {
+    enable = true;
+    package = inputs.swww.packages.${pkgs.system}.swww;
+  };
 }
