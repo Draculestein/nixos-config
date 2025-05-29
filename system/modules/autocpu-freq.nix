1b1d9196e75aca85ae0742fdd4f68e77f7a034eb
Draculestein <albertjuliotheodorus@gmail.com>
2025-05-28 23:25:06 -0600

    Disabled tlp and ppd if auto-cpufreq is enabled

--- system/modules/autocpu-freq.nix
+++ system/modules/autocpu-freq.nix
@@ -1,8 +1,11 @@
-{config, lib, pkgs, inputs, ...}:
+{ config, lib, pkgs, inputs, ... }:
 {
   imports = [
     inputs.auto-cpufreq.nixosModules.default
   ];
 
-  services.auto-cpufreq.enable = true;
+  programs.auto-cpufreq.enable = true;
+  services.power-profiles-daemon.enable = lib.mkForce false;
+  services.tlp.enable = lib.mkForce false;
+
 }
