85e72018f6cc3054397bde19154667eb361404b6
Draculestein <albertjuliotheodorus@gmail.com>
2025-06-10 12:40:39 -0600

    Reverted zswap config since configuring using kernelparams does not work

--- system/modules/zswap.nix
+++ system/modules/zswap.nix
@@ -1,11 +1,18 @@
-{config, pkgs, ...}:
+{ config, pkgs, ... }:
 {
   # Enable zswap with lz4
-  boot.initrd.kernelModules = [ "lz4" ];
+  boot.kernelModules = [ "lz4" ];
   boot.kernelParams = [
     "zswap.enabled=1"
-    "zswap.compressor=lz4"
   ];
-
+  
+  systemd.services.zswap-configure = {
+    description = "Configure zswap";
+    wantedBy = [ "multi-user.target" ];
+    serviceConfig.Type = "oneshot";
+    script = ''
+      echo lz4 > /sys/module/zswap/parameters/compressor
+    '';
+  };
 
 }
