f9cb4be5d3a0c5a2c7c7d7a0b8ea1b73fbf31c6b
Draculestein <albertjuliotheodorus@gmail.com>
2025-06-23 02:14:30 -0600

    Followed tlp's tutorial on hybrid gpu config

--- system/modules/tlp.nix
+++ system/modules/tlp.nix
@@ -5,6 +5,8 @@
   services.tlp.settings = {
     USB_AUTOSUSPEND = 0;
     TLP_DEFAULT_MODE = "BAT";
+    RUNTIME_PM_DRIVER_DENYLIST = "mei_me";
+    RUNTIME_PM_ON_AC = "auto";
     RUNTIME_PM_ENABLE = "64:00.0";
   };
 }
