5ca6a971445a0baa7473530ddc48e5c6de1d5bcd
Draculestein <albertjuliotheodorus@gmail.com>
2025-09-23 02:15:53 -0600

    Added nix cache for nix community and nixpkgs python

--- system/modules/nixSettings.nix
+++ system/modules/nixSettings.nix
@@ -1,5 +1,5 @@
-{ config, lib, pkgs, inputs, ... }: 
-{ 
+{ config, lib, pkgs, inputs, ... }:
+{
 
   nix = {
     nixPath = [ "nixpkgs=${inputs.nixpkgs-unstable}" ];
@@ -7,6 +7,16 @@
       experimental-features = [ "nix-command" "flakes" ];
       auto-optimise-store = true;
       trusted-users = [ "root" ];
+
+      substituters = [
+        "https://nix-community.cachix.org"
+        "https://nixpkgs-python.cachix.org"
+      ];
+      trusted-public-keys = [
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "nixpkgs-python.cachix.org-1:hxjI7pFxTyuTHn2NkvWCrAUcNZLNS3ZAvfYNuYifcEU="
+      ];
+
     };
 
     gc = {
