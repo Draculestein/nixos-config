8e7fb23addda0df723c9609d44aa2ddb4736c46e
Draculestein <albertjuliotheodorus@gmail.com>
2025-09-29 11:33:43 -0600

    Re-enabled stylix

--- users/albertjul/theme.nix
+++ users/albertjul/theme.nix
@@ -1,19 +1,19 @@
 { config, lib, pkgs, ... }:
 {
   stylix = {
-    enable = false;
+    enable = true;
     image = ./desktop.jpg;
 
     polarity = "dark";
     # base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-macchiato.yaml";
 
     cursor = {
-     name = "Capitaine Cursors (Palenight)";
-     package = pkgs.capitaine-cursors-themed;
-     size = 16;
+      name = "Capitaine Cursors (Palenight)";
+      package = pkgs.capitaine-cursors-themed;
+      size = 16;
     };
 
-    fonts = { 
+    fonts = {
       monospace = {
         name = "Hasklig";
         package = pkgs.hasklig;
@@ -64,29 +64,16 @@
   # };
 
   # home.pointerCursor = {
-  #    gtk.enable = true;
-  #    package = pkgs.vimix-cursor-theme;
-  #    name = "Vimix-Cursors";
-  #    size = 12;
+  #   gtk.enable = true;
+  #   name = "Capitaine Cursors (Palenight)";
+  #   package = pkgs.capitaine-cursors-themed;
+  #   size = 16;
   # };
 
   # qt = {
   #   enable = true;
   #   platformTheme.name = "adwaita";
   #   style.name = "adwaita-dark";
-  # };
-
-  # gtk = {
-  #   enable = true;
-  #   theme = {
-  #     package = pkgs.gnome-themes-extra;
-  #     name = "Adwaita-dark";
-  #   };
-
-  #   iconTheme = {
-  #     name = "Papirus";
-  #     package = pkgs.papirus-icon-theme;
-  #   };
   # };
 
 }
