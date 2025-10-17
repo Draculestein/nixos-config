51292060fe8da11aea0f68a1e00181ddfca6d567
Draculestein <albertjuliotheodorus@gmail.com>
2025-10-17 10:17:46 -0600

    Add more config for tlp

--- system/modules/tlp.nix
+++ system/modules/tlp.nix
@@ -4,9 +4,15 @@
   services.tlp.enable = true;
   services.tlp.settings = {
     USB_AUTOSUSPEND = 0;
+    USB_EXCLUDE_BTUSB = 1;
     TLP_DEFAULT_MODE = "BAT";
-    RUNTIME_PM_DRIVER_DENYLIST = "mei_me";
+    RUNTIME_PM_DRIVER_DENYLIST = "mei_me nouveau radeon xhci_hcd amdxdna";
     RUNTIME_PM_ON_AC = "auto";
-    RUNTIME_PM_ENABLE = "64:00.0";
+    RUNTIME_PM_ENABLE = "64:00.0 64:00.1";
+
+    CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+    CPU_ENERGY_PERF_POLICY_ON_AC = "balance_performance";
+    PLATFORM_PROFILE_ON_AC = "balanced";
+    PLATFORM_PROFILE_ON_BAT = "low-power";
   };
 }
