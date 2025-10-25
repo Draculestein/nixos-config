b53cd7de651c23fef05a5a7bffbee262d0bae174
Draculestein <albertjuliotheodorus@gmail.com>
2025-10-25 01:50:09 -0600

    Removed nvidia from tlp

--- system/modules/tlp.nix
+++ system/modules/tlp.nix
@@ -6,7 +6,7 @@
     USB_AUTOSUSPEND = 0;
     USB_EXCLUDE_BTUSB = 1;
     TLP_DEFAULT_MODE = "BAT";
-    RUNTIME_PM_DRIVER_DENYLIST = "mei_me nouveau radeon nvidia xhci_hcd amdxdna";
+    RUNTIME_PM_DRIVER_DENYLIST = "mei_me nouveau xhci_hcd amdxdna";
     RUNTIME_PM_ON_AC = "auto";
     RUNTIME_PM_ENABLE = "64:00.0 64:00.1";
 
