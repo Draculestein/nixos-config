4d6f4296f63a352276d9139adc91760f8fdfef5b
Draculestein <albertjuliotheodorus@gmail.com>
2025-11-14 10:03:56 -0700

    Commented out render drm device

--- users/modules/niri/settings.nix
+++ users/modules/niri/settings.nix
@@ -1,7 +1,7 @@
 { config, lib, pkgs, ... }: {
   programs.niri.settings = {
     debug = {
-      render-drm-device = "/dev/dri/amd-igpu";
+      # render-drm-device = "/dev/dri/amd-igpu";
       honor-xdg-activation-with-invalid-serial = [];
     };
 
@@ -19,9 +19,6 @@
     };
 
     spawn-at-startup = [
-      {
-        command = [ "swww" "restore" ];
-      }
       {
         command = [ "hypridle" ];
       }
