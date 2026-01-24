6049fa25d21ec09a1feb3992a513ee50c028ac1c
Draculestein <albertjuliotheodorus@gmail.com>
2026-01-24 14:59:27 -0700

    Added default config for home manager

--- modules/home-manager.nix
+++ modules/home-manager.nix
@@ -1,25 +1,21 @@
-{ den, inputs, ... }:
+{ den, ... }:
 {
   flake-file.inputs.home-manager = {
     url = "github:nix-community/home-manager/master";
     inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  den.default.includes = [ den._.home-manager den.aspects.homeManager den._.inputs' den._.self' ];
+  den.default.includes = [ den._.home-manager den.aspects.homeManager ];
 
   den.aspects.homeManager.homeManager = { config, ... }: {
-    # This value determines the Home Manager release that your configuration is
-    # compatible with. This helps avoid breakage when a new Home Manager release
-    # introduces backwards incompatible changes.
-    #
-    # You should not change this value, even if you update Home Manager. If you do
-    # want to update the value, then make sure to first check the Home Manager
-    # release notes.
-    home.stateVersion = "23.11"; # Please read the comment before changing.
     targets.genericLinux.enable = true;
     xdg.mime.enable = true;
     xdg.systemDirs.data = [ "${config.home.homeDirectory}/.nix-profile/share/applications" ];
 
+    programs.bash.enable = true;
+    programs.vim.enable = true;
+
+    # Let home-manager install and manage itself
     programs.home-manager.enable = true;
   };
 }
