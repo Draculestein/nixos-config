2e5cbd4daf0cb586ef3c9dd21ed6143014d0c5fb
Draculestein <albertjuliotheodorus@gmail.com>
2026-01-25 00:35:13 -0700

    Added user config

--- modules/users/albertjul.nix
+++ modules/users/albertjul.nix
@@ -1,5 +1,17 @@
 { den, ... }: {
-  den.aspects.albertjul.includes = [
-    den.aspects.code-dev
-  ];
+  den.aspects.albertjul = {
+    includes = [
+      den.aspects.code-dev
+    ];
+
+    nixos = { pkgs, ... }: {
+      users.users.albertjul = {
+        isNormalUser = true;
+        uid = 1000;
+        extraGroups = [ "wheel" "podman" "gamemode" "libvirtd" "video" "dialout" "scanner" "lp" ]; # Enable ‘sudo’ for the user.
+        packages = [ ]; # Apps are handled by home-manager
+        shell = pkgs.zsh;
+      };
+    };
+  };
 }
