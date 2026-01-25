668ca9257614c37df9313bfde2c3971a4838ea5a
Draculestein <albertjuliotheodorus@gmail.com>
2026-01-24 19:37:08 -0700

    Simplified host declaration

--- modules/hosts.nix
+++ modules/hosts.nix
@@ -1,13 +1,8 @@
 {
   den.hosts.x86_64-linux = {
     AlbertProP16 = {
-      name = "AlbertProP16";
       description = "ASUS H7606";
-      users = {
-        albertjul = {
-          userName = "albertjul";
-        };
-      };
+      users.albertjul = { };
     };
   };
 }
