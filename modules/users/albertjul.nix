4efff6b2eb674556117befaf3cbe16daef09fdb5
Draculestein <albertjuliotheodorus@gmail.com>
2026-01-25 11:27:24 -0700

    Adjusted stylix config

--- modules/users/albertjul.nix
+++ modules/users/albertjul.nix
@@ -114,6 +114,61 @@
           "${pkgs.thunderbird}/share/applications/thunderbird.desktop"
         ];
       };
+
+      homeManager = { pkgs, ... }: {
+        stylix = {
+          enable = true;
+          image = ./desktop.jpg;
+
+          polarity = "dark";
+
+          cursor = {
+            name = "Capitaine Cursors (Palenight)";
+            package = pkgs.capitaine-cursors-themed;
+            size = 16;
+          };
+
+          fonts = {
+            monospace = {
+              name = "Hasklig";
+              package = pkgs.hasklig;
+            };
+
+            serif = {
+              name = "Adwaita Sans";
+              package = pkgs.adwaita-fonts;
+            };
+
+            sansSerif = {
+              name = "Adwaita Sans";
+              package = pkgs.adwaita-fonts;
+            };
+
+            sizes = {
+              applications = 10;
+              desktop = 10;
+              popups = 10;
+              terminal = 12;
+            };
+          };
+
+          icons = {
+            enable = true;
+            package = pkgs.papirus-icon-theme;
+            dark = "Papirus";
+            light = "Papirus-Light";
+          };
+
+          targets = {
+            vscode.enable = false;
+            hyprpaper.enable = true;
+            rofi.enable = false;
+            waybar.enable = false;
+            spicetify.enable = false;
+            kitty.enable = false;
+          };
+        };
+      };
     };
   };
 }
