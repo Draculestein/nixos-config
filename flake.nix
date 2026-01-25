f00064bc3727b2b49b71d774e677a127d21e50a5
Draculestein <albertjuliotheodorus@gmail.com>
2026-01-25 11:31:03 -0700

    Updated flake.nix

--- flake.nix
+++ flake.nix
@@ -5,6 +5,10 @@
   outputs = inputs: inputs.flake-parts.lib.mkFlake { inherit inputs; } (inputs.import-tree ./modules);
 
   inputs = {
+    autocpu-freq = {
+      inputs.nixpkgs.follows = "nixpkgs";
+      url = "github:AdnanHodzic/auto-cpufreq";
+    };
     config-secrets = {
       flake = false;
       url = "git+ssh://git@github.com/Draculestein/nixos-secrets.git?ref=main&shallow=1";
@@ -14,6 +18,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
       url = "github:nix-community/disko";
     };
+    dwproton.url = "github:imaviso/dwproton-flake";
     flake-aspects.url = "github:vic/flake-aspects";
     flake-file.url = "github:vic/flake-file";
     flake-parts = {
@@ -25,9 +30,23 @@
       url = "github:nix-community/home-manager/master";
     };
     import-tree.url = "github:vic/import-tree";
+    niri = {
+      inputs.nixpkgs.follows = "nixpkgs";
+      url = "github:sodiboo/niri-flake";
+    };
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     nixpkgs-lib.follows = "nixpkgs";
+    noctalia = {
+      inputs.nixpkgs.follows = "nixpkgs";
+      url = "github:noctalia-dev/noctalia-shell";
+    };
+    sops-nix = {
+      inputs.nixpkgs.follows = "nixpkgs";
+      url = "github:Mic92/sops-nix";
+    };
+    spicetify-nix.url = "github:Gerg-L/spicetify-nix";
+    stylix.url = "github:nix-community/stylix";
     systems.url = "github:nix-systems/default";
     ucodenix.url = "github:e-tho/ucodenix";
   };
