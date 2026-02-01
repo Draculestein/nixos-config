414b488df8f8312e26346a5873d9b09cf9b37b35
Draculestein <albertjuliotheodorus@gmail.com>
2026-02-01 12:18:38 -0700

    Setup hypridle through HM

--- modules/aspects/niri/default.nix
+++ modules/aspects/niri/default.nix
@@ -27,7 +27,7 @@
       security.pam.services.hyprlock = { };
     };
 
-    homeManager = { config, lib, pkgs, ... }:
+    homeManager = { config, lib, pkgs, osConfig, ... }:
       let
         cycle-keyboard-backlight = pkgs.writeShellApplication {
           name = "cycle-kbd-backlight";
@@ -46,6 +46,9 @@
             brightnessctl -d asus::kbd_backlight set "$next"
           '';
         };
+        hostname = osConfig.networking.hostName;
+        hasAsusKbdBacklight = hostname == "AlbertProP16";
+        kbdIdleTimeout = if hostname == "AlbertProP16" then 150 else 300;
       in
       {
         home.packages = [
@@ -54,20 +57,58 @@
           inputs.noctalia.packages.${pkgs.stdenv.hostPlatform.system}.default
           pkgs.nerd-fonts.jetbrains-mono
           pkgs.inter
-          pkgs.hypridle
-          cycle-keyboard-backlight
-        ];
+        ] ++ lib.optionals hasAsusKbdBacklight [ cycle-keyboard-backlight ];
 
-        stylix.targets.hyprlock.enable = false;
-
-        home.file.".config/hypr/hypridle.conf" = {
+        services.hypridle = {
           enable = true;
-          source = ./hypridle.conf;
+          settings = {
+            general = {
+              lock_cmd = "pidof hyprlock || noctalia-shell ipc call lockScreen lock"; # avoid starting multiple hyprlock instances.
+              before_sleep_cmd = "loginctl lock-session"; # lock before suspend.
+              after_sleep_cmd = "niri msg action power-on-monitors"; # to avoid having to press a key twice to turn on the display.
+            };
+
+            listener = [
+              {
+                timeout = if hasAsusKbdBacklight then 150 else 300; # 2.5min.
+                on-timeout = "brightnessctl -s set 1%"; # set monitor backlight to minimum, avoid 0 on OLED monitor.
+                on-resume = "brightnessctl -r"; # monitor backlight restore.
+              }
+
+              {
+                timeout = 300; # 5min
+                on-timeout = "systemd-ac-power || loginctl lock-session"; # lock screen when timeout has passed
+              }
+
+              {
+                timeout = 330; # 5.5min
+                on-timeout = "niri msg action power-off-monitors"; # screen off when timeout has passed
+                on-resume = "niri msg action power-on-monitors && brightnessctl -r"; # screen on when activity is detected after timeout has fired.
+              }
+
+              {
+                timeout = 1200; # 20min
+                on-timeout = "systemd-ac-power || systemctl suspend"; # suspend pc
+              }
+            ] ++ lib.optionals hasAsusKbdBacklight [
+              {
+                timeout = kbdIdleTimeout;
+                on-timeout = "brightnessctl -sd asus::kbd_backlight set 0";
+                on-resume = "brightnessctl -rd asus::kbd_backlight";
+              }
+            ];
+
+          };
+        };
+
+        systemd.user.services.hypridle = {
+          Unit.PartOf = [ "niri.service" ];
+          Install.WantedBy = [ "niri.service" ];
         };
 
         services.gammastep = {
           enable = true;
-          enableVerboseLogging = true;
+          enableVerboseLogging = false;
           provider = "geoclue2";
           temperature = { day = 6500; night = 3000; };
           settings = {
