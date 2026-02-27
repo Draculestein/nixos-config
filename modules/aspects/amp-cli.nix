41e68a61645dcf87bc7f6e3a0fb4a590d0ce86b2
Draculestein <albertjuliotheodorus@gmail.com>
2026-02-27 08:24:39 -0700

    Fixed amp settings

--- modules/aspects/amp-cli.nix
+++ modules/aspects/amp-cli.nix
@@ -6,14 +6,17 @@
       home.file.".config/amp/settings.json" = {
         enable = true;
         text = ''
-          "amp.defaultVisibility": {
-            "github.com/heavyset-tech/heavyset-api": "private",
-            "github.com/heavyset-tech/heavyset-admin": "private",
-            "github.com/heavyset-tech/heavyset-apt-ui-classic-1": "private",
-            "github.com/heavyset-tech/heavyset-scripts": "private",
-          },
-          "amp.git.commit.ampThread.enabled": false,
-          "amp.git.commit.coauthor.enabled": false
+          {
+            "amp.defaultVisibility": {
+              "github.com/heavyset-tech/heavyset-api": "private",
+              "github.com/heavyset-tech/heavyset-admin": "private",
+              "github.com/heavyset-tech/heavyset-apt-ui-classic-1": "private",
+              "github.com/heavyset-tech/heavyset-scripts": "private"
+            },
+            "amp.git.commit.ampThread.enabled": false,
+            "amp.git.commit.coauthor.enabled": false
+          }
+
         '';
       };
     };
