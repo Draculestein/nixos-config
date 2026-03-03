735e22519afcb168c282cadd0aba235c7f0bbbe6
Draculestein <albertjuliotheodorus@gmail.com>
2026-03-03 14:31:40 -0700

    Added rog-control-center as a systemd service

--- modules/aspects/asus.nix
+++ modules/aspects/asus.nix
@@ -1,6 +1,6 @@
 { den, ... }:
 {
-  den.aspects.asus.nixos = { lib, ... }: {
+  den.aspects.asus.nixos = { lib, pkgs, ... }: {
     services.asusd = {
       enable = true;
     };
@@ -11,7 +11,18 @@
 
     programs.rog-control-center = {
       enable = true;
-      autoStart = true;
+    };
+
+    systemd.user.services.rog-control-center = {
+      description = "ASUS ROG Control Center";
+      wantedBy = [ "graphical-session.target" ];
+      partOf = [ "graphical-session.target" ];
+      after = [ "graphical-session.target" ];
+      serviceConfig = {
+        ExecStart = "${lib.getExe' pkgs.asusctl "rog-control-center"}";
+        Restart = "on-failure";
+        RestartSec = 5;
+      };
     };
 
     services.power-profiles-daemon.enable = lib.mkForce true;
