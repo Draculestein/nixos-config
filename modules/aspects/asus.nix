4ee81181495a94a0c52782cda1180869dc332954
Draculestein <albertjuliotheodorus@gmail.com>
2026-03-22 15:31:04 -0600

    Simplified rog-control-center config

--- modules/aspects/asus.nix
+++ modules/aspects/asus.nix
@@ -11,18 +11,7 @@
 
     programs.rog-control-center = {
       enable = true;
-    };
-
-    systemd.user.services.rog-control-center = {
-      description = "ASUS ROG Control Center";
-      wantedBy = [ "graphical-session.target" ];
-      partOf = [ "graphical-session.target" ];
-      after = [ "graphical-session.target" ];
-      serviceConfig = {
-        ExecStart = "${lib.getExe' pkgs.asusctl "rog-control-center"}";
-        Restart = "on-failure";
-        RestartSec = 5;
-      };
+      autoStart = true;
     };
 
     services.power-profiles-daemon.enable = lib.mkForce true;
