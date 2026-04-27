e5009207cd486f9d84985b301c162bd383f9684b
Draculestein <albertjuliotheodorus@gmail.com>
2026-04-27 08:45:18 -0600

    Updated flake lock

--- modules/users/albertjul.nix
+++ modules/users/albertjul.nix
@@ -72,7 +72,21 @@
         vesktop
         rnote
         gnome-disk-utility
-        bottles
+        (bottles.override {
+          # Intercept buildFHSEnv to modify target packages
+          buildFHSEnv = args: pkgs.buildFHSEnv (args // {
+            multiPkgs = envPkgs:
+              let
+                # Fetch original package list
+                originalPkgs = args.multiPkgs envPkgs;
+
+                # Disable tests for openldap
+                customLdap = envPkgs.openldap.overrideAttrs (_: { doCheck = false; });
+              in
+              # Replace broken openldap with the custom one
+              builtins.filter (p: (p.pname or "") != "openldap") originalPkgs ++ [ customLdap ];
+          });
+        })
         nautilus
         sushi
         image-roll
