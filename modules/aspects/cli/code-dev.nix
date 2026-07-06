3b9776ce733f34a3081dc7edfe2bd44a92c86b01
Draculestein <albertjuliotheodorus@gmail.com>
2026-07-06 15:25:10 -0600

    Updated devenv

--- modules/aspects/cli/code-dev.nix
+++ modules/aspects/cli/code-dev.nix
@@ -22,7 +22,25 @@
     };
 
     homeManager = { pkgs, ... }: {
-      home.packages = [ pkgs.devenv ];
+      home.packages = [
+        (pkgs.devenv.overrideAttrs (
+          final: prev: {
+            src = pkgs.fetchFromGitHub {
+              owner = "cachix";
+              repo = "devenv";
+              rev = "c0174f8e7d46619bca28eb2be1a7ef3cca261be6";
+              hash = "sha256-kG+DE1Slci5Buio0opArMiRkbf4n697m8U/O2kPuQOw=";
+            };
+            cargoDeps = prev.cargoDeps.overrideAttrs (prev: {
+              # https://discourse.nixos.org/t/is-it-possible-to-override-cargosha256-in-buildrustpackage/4393/25
+              vendorStaging = prev.vendorStaging.overrideAttrs {
+                src = final.src;
+                outputHash = "sha256-30VwKVx4b2vrHlzPq96EwHDfQSfcz+CauLZhOl1TbyQ=";
+              };
+            });
+          }
+        ))
+      ];
     };
   };
 }
