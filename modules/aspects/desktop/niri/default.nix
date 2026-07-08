fc3dcc2bd8002c8af2ceee3c0eed82c3fafb0705
Draculestein <albertjuliotheodorus@gmail.com>
2026-07-08 10:46:37 -0600

    Move to KDL rather than sodiboo home manager

--- modules/aspects/desktop/niri/default.nix
+++ modules/aspects/desktop/niri/default.nix
@@ -18,8 +18,6 @@
 
   den.aspects.niri = {
     includes = [
-      den.aspects.niri._.keybinds
-      den.aspects.niri._.window-rules
       den.aspects.niri._.idle
     ];
 
@@ -45,6 +43,26 @@
     };
 
     homeManager = { config, lib, pkgs, osConfig, ... }:
+      let
+        cycle-keyboard-backlight = pkgs.writeShellApplication {
+          name = "cycle-kbd-backlight";
+          runtimeInputs = [
+            pkgs.brightnessctl
+          ];
+          text = ''
+            # Get the current keyboard backlight level
+            current=$(brightnessctl -d asus::kbd_backlight get)
+            max=$(brightnessctl -d asus::kbd_backlight max)
+
+            # Calculate the next level (cycle from 0 to 3)
+            next=$(( (current + 1) % (max + 1) ))
+
+            # Set the new backlight level
+            brightnessctl -d asus::kbd_backlight set "$next"
+          '';
+        };
+        hasAsusKbdBacklight = osConfig.networking.hostName == "AlbertProP16";
+      in
       {
         imports = [
           inputs.nfsm-flake.homeModules.default
@@ -61,7 +79,9 @@
           pkgs.wl-clipboard
           pkgs.satty
           pkgs.polkit_gnome
-        ];
+
+        ] ++ lib.optionals hasAsusKbdBacklight [ cycle-keyboard-backlight ];
+
 
         services.gammastep = {
           enable = true;
@@ -151,89 +171,13 @@
           };
         };
 
-        programs.niri.settings = {
-          debug = {
-            render-drm-device = "/dev/dri/amd-igpu";
-            honor-xdg-activation-with-invalid-serial = [ ];
-          };
-
-          environment = {
-            GDK_BACKEND = "wayland,x11,*";
-            SDL_VIDEODRIVER = "wayland,x11";
-            CLUTTER_BACKEND = "wayland";
-            QT_QPA_PLATFORM = "wayland;xcb";
-            QT_AUTO_SCREEN_SCALE_FACTOR = "1";
-            QT_QPA_PLATFORMTHEME = "qt6ct;qt5ct";
-            ELECTRON_OZONE_PLATFORM_HINT = "auto";
-          };
-
-          spawn-at-startup = [
-            { command = [ "noctalia" ]; }
-          ];
-
-          input = {
-            keyboard = { xkb.layout = "us"; };
-            touchpad = { enable = true; tap = true; dwt = true; natural-scroll = true; };
-            mouse = { enable = true; };
-            trackpoint = { enable = true; };
-            touch = { map-to-output = "eDP-1"; };
-            tablet = { enable = true; map-to-output = "eDP-1"; };
-            focus-follows-mouse = { enable = true; max-scroll-amount = "0%"; };
-          };
-
-          outputs = {
-            "eDP-1" = {
-              enable = true;
-              scale = 2.0;
-              mode = { width = 3840; height = 2400; refresh = 60.00; };
-              position = { x = 1920; y = 0; };
-            };
-            "ASUSTek COMPUTER INC VG278 M3LMQS154329" = {
-              enable = true;
-              variable-refresh-rate = "on-demand";
-              scale = 1.0;
-              mode = { width = 1920; height = 1080; refresh = 164.917; };
-              position = { x = 0; y = 0; };
-            };
-
-            "HP Inc. HP M24h 3CM3120S60" = {
-              enable = true;
-              scale = 1.0;
-              mode = { width = 1920; height = 1080; refresh = 60.000; };
-              position = { x = -1080; y = 0; };
-              transform.rotation = 90;
-            };
-          };
-
-          layout = {
-            gaps = 10;
-            background-color = "transparent";
-            focus-ring = { enable = true; width = 2; };
-            border = { enable = true; width = 2; };
-            struts = { left = 10; right = 10; top = 0; bottom = 2; };
-            tab-indicator = { place-within-column = true; };
-          };
-
-          workspaces = {
-            "media" = { open-on-output = "eDP-1"; };
-            "personal-monitor" = {
-              open-on-output = "ASUSTek COMPUTER INC VG278 M3LMQS154329";
-            };
-            "personal-secondary" = { open-on-output = "eDP-1"; };
-            "work-monitor" = {
-              open-on-output = "ASUSTek COMPUTER INC VG278 M3LMQS154329";
-            };
-            "work-secondary" = { open-on-output = "eDP-1"; };
-          };
-
-          animations = { enable = true; slowdown = 0.9; };
-          clipboard.disable-primary = true;
-          cursor = {
-            theme = "Capitaine Cursors (Palenight)";
-            size = 16;
-            hide-when-typing = true;
-          };
-          hotkey-overlay.skip-at-startup = true;
+        xdg.configFile = {
+          "niri/config.kdl".source = config.lib.file.mkOutOfStoreSymlink
+            "${config.home.homeDirectory}/.dotfiles/modules/aspects/desktop/niri/config.kdl";
+          "niri/keybinds.kdl".source = config.lib.file.mkOutOfStoreSymlink
+            "${config.home.homeDirectory}/.dotfiles/modules/aspects/desktop/niri/keybinds.kdl";
+          "niri/window-rules.kdl".source = config.lib.file.mkOutOfStoreSymlink
+            "${config.home.homeDirectory}/.dotfiles/modules/aspects/desktop/niri/window-rules.kdl";
         };
       };
   };
