9d165caa75d0a7fd1ae05905aaefc7dbc603f409
Draculestein <albertjuliotheodorus@gmail.com>
2026-08-03 11:25:37 -0600

    Remove fast-flow

--- modules/aspects/hardware/amd-npu.nix
+++ modules/aspects/hardware/amd-npu.nix
@@ -9,6 +9,10 @@
       hardware.amd-npu = {
         enable = true;
         enableLemonade = lib.mkDefault false;
+        enableFastFlowLM = lib.mkDefault false;
+        enableROCm = lib.mkDefault false; # ROCm GPU backends (llamacpp + sd-cpp)
+        enableVulkan = lib.mkDefault false; # Vulkan GPU backends (llamacpp + whispercpp)
+        enableImageGen = lib.mkDefault false;
       };
 
     };
