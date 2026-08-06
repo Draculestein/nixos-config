846ef57f142e8bda5a247e14d273654257a36fad
Draculestein <albertjuliotheodorus@gmail.com>
2026-08-06 10:34:15 -0600

    Add ddcci service

--- modules/hosts/AlbertProP16/configuration.nix
+++ modules/hosts/AlbertProP16/configuration.nix
@@ -35,7 +35,38 @@
     hardware.enableRedistributableFirmware = true;
     hardware.enableAllFirmware = true;
     hardware.sensor.iio.enable = true;
-    hardware.i2c.enable = true;
+
+    services.ddccontrol.enable = true;
+    services.udev.extraRules = ''
+      SUBSYSTEM=="i2c-dev", ACTION=="add", ATTR{name}=="NVIDIA i2c adapter*", TAG+="ddcci", TAG+="systemd", ENV{SYSTEMD_WANTS}+="ddcci@$kernel.service"
+      SUBSYSTEM=="i2c-dev", ACTION=="add", ATTR{name}=="AMDGPU DM aux*", TAG+="ddcci", TAG+="systemd", ENV{SYSTEMD_WANTS}+="ddcci@$kernel.service"
+      SUBSYSTEM=="i2c-dev", ACTION=="add", ATTR{name}=="AMDGPU DM i2c*", TAG+="ddcci", TAG+="systemd", ENV{SYSTEMD_WANTS}+="ddcci@$kernel.service"
+    '';
+
+    systemd.services."ddcci@" = {
+      scriptArgs = "%i";
+      after = [ "graphical.target" ];
+      path = [
+        pkgs.i2c-tools
+      ];
+      script = ''
+        echo "Trying to attach ddcci to $1"
+        id=$(echo $1 | cut -d "-" -f 2)
+        echo "id: $id"
+
+        if ! i2cdetect -y $id 0x37 0x37 | grep -q 37; then
+          echo "Cannot find 0x37 device"
+          exit 0
+        fi
+
+        echo "ddcci 0x37" > /sys/bus/i2c/devices/$1/new_device && echo "Successfully attached ddcci to $1" || echo "Failed to attach"
+      '';
+
+      serviceConfig = {
+        Type = "oneshot";
+        Restart = "no";
+      };
+    };
 
     hardware.bluetooth = {
       enable = true;
