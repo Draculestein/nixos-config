6605d00c9714c3e15b440a098ce526558e07067a
Draculestein <albertjuliotheodorus@gmail.com>
2026-09-20 00:03:46 -0600

    Default basic config to boot

---
+++ modules/hosts/heimdall/default.nix
@@ -0,0 +1,31 @@
+{ den, inputs, ... }:
+{
+  den.aspects.heimdall = {
+    includes = [
+      den._.hostname
+    ];
+
+    nixos = { config, lib, pkgs, modulesPath, ... }: {
+      imports = [
+        (modulesPath + "/installer/scan/not-detected.nix")
+        inputs.nixos-hardware.nixosModules.common-cpu-intel
+        inputs.nixos-hardware.nixosModules.common-pc-ssd
+      ];
+
+      boot = {
+        loader.systemd-boot.enable = true;
+        loader.efi.canTouchEfiVariables = true;
+
+        initrd.availableKernelModules = [ "xhci_pci" "ahci" "nvme" "usbhid" "usb_storage" "sd_mod" ];
+        initrd.kernelModules = [ ];
+        kernelModules = [ "kvm-intel" ];
+        extraModulePackages = [ ];
+      };
+
+      nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+
+      hardware.enableRedistributableFirmware = true;
+      networking.networkmanager.enable = true;
+    };
+  };
+}
