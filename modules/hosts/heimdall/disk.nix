7b21310a2cfd5a9992787960a51e727a1fb15f2f
Draculestein <albertjuliotheodorus@gmail.com>
2026-09-20 00:21:18 -0600

    Added disk config

---
+++ modules/hosts/heimdall/disk.nix
@@ -0,0 +1,40 @@
+{ den, inputs, ... }:
+{
+  den.aspects.heimdall.nixos = {
+    imports = [
+      inputs.disko.nixosModules.default
+    ];
+
+    disko.devices.disk.main = {
+      type = "disk";
+      device = "/dev/nvme0n1";
+      content = {
+        type = "gpt";
+        partitions = {
+          ESP = {
+            priority = 1;
+            name = "ESP";
+            start = "1M";
+            end = "1G";
+            type = "EF00";
+            content = {
+              type = "filesystem";
+              format = "vfat";
+              mountpoint = "/boot";
+              mountOptions = [ "umask=0077" ];
+            };
+          };
+
+          root = {
+            size = "100%";
+            content = {
+              type = "filesystem";
+              format = "ext4";
+              mountpoint = "/";
+            };
+          };
+        };
+      };
+    };
+  };
+}
